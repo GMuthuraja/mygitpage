bd49c9e1d7090f6c6ea2a2a022c01085da53e20f
Muthu <muthuraja0043@gmail.com>
2023-09-14 12:27:18 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ang_app';
+
+  constructor(private modalService: NgbModal) {
+    console.log("I am a constructor");
+  }
+
+  ngOnChanges() {
+    console.log("I am ngOnChanges");
+  }
+
+  ngOnInit() {
+    console.log("I am ngOnInit");
+  }
+
+  ngDoCheck() {
+    console.log("I am ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("I am ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("I am ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("I am ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("I am ngAfterViewChecked");
+  }
+
+  ngDoDestroy() {
+    console.log("I am ngDoCheck");
+  }
+
+  open(modal: any) {
+    this.modalService.open(modal);
+  }
 }
